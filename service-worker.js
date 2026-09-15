15923bd4b6d8dc30cf4e35f9ebdfeb40112886c3
sarronadrien-sketch <sarron.adrien@orange.fr>
2026-09-15 22:11:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-const CACHE='assmat-v3';
+const CACHE='assmat-v4';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
